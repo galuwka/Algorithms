73e42e35e8c064e3a18bf7bccf903a377f8c763c
galuwka <galinasale@gmail.com>
2015-11-17 15:20:56 +0200

    Create mergesort.rb

---
+++ mergesort.rb
@@ -0,0 +1,10 @@
+def merge_sort(lists)
+  return lists if lists.count == 1
+ 
+  middle  = lists[0..(lists.count / 2) - 1 ]
+  left = lists[0..middle.count - 1]
+  right = lists[middle.count..lists.count]
+ 
+  x = merge_sort(left)
+  y = merge_sort(right)
+end
